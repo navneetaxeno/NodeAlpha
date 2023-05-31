720df536c7c298ab18a675d2436c1208bddcf967
vishnavneet <navneet.kumar@axeno.co>
2023-05-31 11:53:29 +0530

    added path using root

--- routes/shop.js
+++ routes/shop.js
@@ -1,25 +1,13 @@
 const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-// 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, './', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
-
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, './', 'views', 'add-product.html'));
-});
-
-// add products : POST
-router.post('/add-product', (req, res, next) => {
-  console.log('response: ', req.body)
-  res.redirect('/');
-});
-
-router.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
-});
 module.exports = router;
